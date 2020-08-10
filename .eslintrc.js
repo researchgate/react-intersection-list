4d57c49ec3e2059b39f69876890f068a9da014cb
Daniel Tschinder <duck+github@mailbox.org>
2020-08-10 10:50:44 +0000

    chore: Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,3 @@
-// Keep this file for editor support
-module.exports = require('@researchgate/spire-config/eslint/react');
+'use strict';
+// This file was created by spire-plugin-eslint for editor support
+module.exports = require('@researchgate/spire-config/eslint/react-typescript');
